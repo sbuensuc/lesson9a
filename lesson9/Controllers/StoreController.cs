85ca5263c92910288acae376176178878c03acb0
Sam <Sam@DESKTOP-O0S9A91>
2016-11-28 11:03:47 -0500

    added store controller and views

---
+++ lesson9/Controllers/StoreController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using lesson9.Models;
+
+namespace lesson9.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreContext db = new MVCMusicStoreContext();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre("Disco"),
+                new Genre("Jazz"),
+                new Genre("Rock")
+            };
+
+
+            return View(genres);
+        }
+
+        //Get: Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            if (genre == String.Empty)
+            {
+                genre = "Disco";
+
+                Genre genreModel = new Genre(genre);
+            }
+            return View(genreModel);
+        }
+
+        // GET: Store/Details/5
+        public ActionResult Details(int? id = 1)
+        {
+            Album album = new Album("Ablum " + id);
+            
+            return View(album);
+        }
+
+        
+    }
+}
