f8f4d87a53fa3c30b3e5c588d310759d2633b318
Sam <Sam@DESKTOP-O0S9A91>
2016-11-28 10:00:14 -0500

    Add project files.

---
+++ lesson9/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lesson9.Startup))]
+namespace lesson9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
